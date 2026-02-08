1e40dea4c5376c5fcdd3e3076d33dd1782e16bf0
SameerShaik786 <littlesameer2020@gmail.com>
2026-02-08 23:07:55 +0530

    Network error updated

--- modules/projects/components/ProjectHeader.jsx
+++ modules/projects/components/ProjectHeader.jsx
@@ -15,14 +15,14 @@ import {
     DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
 import { Spinner } from "@/components/ui/spinner";
-import Link from "next/link"; 
+import Link from "next/link";
 import { ChevronDownIcon, ChevronLeftIcon, MoveDownIcon, MoveDownLeftIcon, MoveLeftIcon, SunMoonIcon } from 'lucide-react';
 import { useTheme } from "next-themes";
 
 
 const ProjectHeader = ({ project, isPending }) => {
 
-    const {setTheme,theme} = useTheme()
+    const { setTheme, theme } = useTheme()
     return (
         <div className="p-2 flex justify-between items-center border-b text-sm bg-black-700">
             <DropdownMenu>
@@ -36,30 +36,29 @@ const ProjectHeader = ({ project, isPending }) => {
                             className='items-center shrink:0 invert dark:invert-0'
                         />
                         <span className='text-md p-2'>{isPending ? <Spinner /> : project.name}</span>
-                        <ChevronDownIcon className="size-4"/>
+                        <ChevronDownIcon className="size-4" />
                     </Button>
                 </DropdownMenuTrigger>
                 <DropdownMenuContent className={"text-sm w-[180]"} size={"left"} align={"start"}>
                     <DropdownMenuGroup>
                         <DropdownMenuItem className={"text-sm"}>
                             <Link href={"/"} className="flex gap-2 items-center">
-                                <span><ChevronLeftIcon size="4"/></span>
-                                    Go to Dashboard
+                                <span><ChevronLeftIcon size="4" /></span>
+                                Go to Dashboard
                             </Link>
                         </DropdownMenuItem>
                         <DropdownMenuSeparator />
                         <DropdownMenuSub>
                             <DropdownMenuSubTrigger className={"cursor-pointer"}>
-                                <SunMoonIcon size="4"/>Appearance</DropdownMenuSubTrigger>
+                                <SunMoonIcon size="4" />Appearance</DropdownMenuSubTrigger>
                             <DropdownMenuPortal>
                                 <DropdownMenuSubContent>
-                                    <DropdownMenuItem onClick = {() =>setTheme("light") } className={"cursor-pointer"}>Light</DropdownMenuItem>
-                                    <DropdownMenuItem onClick = {() => setTheme("dark")} className={"cursor-pointer"}>Dark</DropdownMenuItem>
-                                    <DropdownMenuItem onClick = {() => setTheme("system")} className={"cursor-pointer"}>System</DropdownMenuItem>
+                                    <DropdownMenuItem onClick={() => setTheme("light")} className={"cursor-pointer"}>Light</DropdownMenuItem>
+                                    <DropdownMenuItem onClick={() => setTheme("dark")} className={"cursor-pointer"}>Dark</DropdownMenuItem>
+                                    <DropdownMenuItem onClick={() => setTheme("system")} className={"cursor-pointer"}>System</DropdownMenuItem>
                                 </DropdownMenuSubContent>
                             </DropdownMenuPortal>
                         </DropdownMenuSub>
-                        <DropdownMenuSeparator />
                     </DropdownMenuGroup>
                 </DropdownMenuContent>
             </DropdownMenu>
